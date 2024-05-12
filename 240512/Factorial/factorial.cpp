908126a7fa1089a99b1e42b5e2a09b9c5c15453b
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-05-12 22:18:52 +0900

    Title: Factorial, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240512/Factorial/factorial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int fac(int n) {
+    if (n <= 1) return 1;
+    return fac(n-1) * n;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fac(n);
+    return 0;
+}
