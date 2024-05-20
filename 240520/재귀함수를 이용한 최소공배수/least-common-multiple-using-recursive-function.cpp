c9b06caa4a6796d4834206d607b5b8918232a6c1
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-05-20 15:44:32 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240520/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int GCD(int a, int b) {
+    for (int i=2; i<=a; i++) {
+        if (a%i==0 && b%i==0) return i;
+    }
+    return 1;
+}
+
+int LCM(int a, int b) {
+    return a*b/GCD(a,b);
+}
+
+int get_LCMs(int n) {
+    if (n==1) return 1;
+    int new_n;
+    cin >> new_n;
+    return LCM(new_n, get_LCMs(n-1));
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << get_LCMs(n);
+    return 0;
+}
