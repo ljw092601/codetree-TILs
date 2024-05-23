8a1198e141d2e960fb7cfddf8f0a990237654920
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-05-23 14:40:47 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240523/DateTime to DateTime/datetime-to-datetime.cpp
+++ 240523/DateTime to DateTime/datetime-to-datetime.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     int a,b,c;
     cin >> a >> b >> c;
-    cout << (a*24*60 + b*60 + c) - (11*24*60 + 11*60 + 11);
+    int min = (a*24*60 + b*60 + c) - (11*24*60 + 11*60 + 11);
+    cout << ((min >= 0) ? min : -1);
     return 0;
 }
