5a33687581945f642c7f752d4c7c0065d99d8f39
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-05-23 16:01:46 +0900

    Title: 10진수로 변환하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240523/10진수로 변환하기/convert-to-decimal.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int n_to_decimal (int n, int nums) {
+    int decimal = 0;
+    int temp = nums;
+    for(int i=0; pow(10, i)<nums; i++) {
+        decimal += (temp%10) * pow(n, i);
+        temp /= 10;
+    }
+    return decimal;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n_to_decimal(2, n);
+    return 0;
+}
