b864c7b7348420f6e774d8e847706439d1a16141
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-05-23 16:03:15 +0900

    Title: 진수 to 진수, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240523/진수 to 진수/transformation-of-number-system.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int n_to_decimal (int n, int nums) {
+    int decimal = 0;
+    int temp = nums;
+    for(int i=0; pow(10, i)<=nums; i++) {
+        decimal += (temp%10) * pow(n, i);
+        temp /= 10;
+    }
+    return decimal;
+}
+
+int main() {
+    int a, b, n;
+    cin >> a >> b >> n;
+    int de = n_to_decimal(a, n);
+    vector<int> bin_vec;
+    while(true) {
+        if (de<2) {
+            bin_vec.emplace_back(de);
+            break; 
+        }
+        bin_vec.emplace_back(de%2);
+        de /= 2;
+    }
+    for (int i= bin_vec.size()-1; i >= 0; i--) cout << bin_vec[i];
+    return 0;
+}
