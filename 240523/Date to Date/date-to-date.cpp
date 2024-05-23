6b15cf4e9e08ca71dda6a2e990f6796577293b55
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-05-23 14:33:18 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240523/Date to Date/date-to-date.cpp
+++ 240523/Date to Date/date-to-date.cpp
@@ -6,10 +6,10 @@ int main() {
     int m1,d1,m2,d2;
     cin >> m1 >> d1 >> m2 >> d2;
     int day = 0;
-    for (int i = m1+1; i != m2; i++) {
+    for (int i = m1; i != m2; i++) {
         day += num_of_days[i];
     }
-    day += num_of_days[m1] - d1 + 1;
+    day -= d1 - 1;
     day += d2;
     cout << day;
     return 0;
