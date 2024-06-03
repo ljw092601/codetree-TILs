aaac5b6f4c0662bc9c715fab7c4a048ceadd2252
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-06-03 15:41:53 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240603/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<char>> board;
+    for (int i=0; i<n; i++) {
+        vector<char> chess;
+        for (int p=0; p<m; p++) {
+            char c;
+            cin >> c;
+            chess.push_back(c);
+        }
+        board.push_back(chess);
+    }
+    int possible = 0;
+    for (int i=1; i<n-2; i++) {
+        for (int p=1; p<n-2; p++) {
+            if (board[0][0] != board[i][p]) {
+                for(int t=i+1; t<n-1; t++) {
+                    for(int r=p+1; r<n-1; r++) {
+                        if (board[0][0] == board[t][r]) possible++;
+                    }
+                }
+            }
+        }
+    }
+    cout << possible;
+    return 0;
+}
