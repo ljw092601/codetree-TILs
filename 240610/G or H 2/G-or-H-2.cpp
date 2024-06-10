a151d0a81f791f5a37367d1ebaacba7ee592fe95
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-06-10 16:07:28 +0900

    Title: G or H 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240610/G or H 2/G-or-H-2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    char arr[110] = {'\0',};
+    while(n--) {
+        int idx;
+        char c;
+        cin >> idx >> c;
+        arr[idx] = c;
+    }
+    int max_dist = 0;
+    for(int i=0; i<100; i++) {
+        for(int p=0; p<100-i; p++) {
+            int cnt_G = 0, cnt_H = 0;
+            int first=0, end=0;
+            bool find_first = false;
+            for(int k=p; k<=p+i; k++) {
+                if(!find_first) {
+                    if(arr[k] == 'G' || arr[k] == 'H') {
+                        first = k;
+                        find_first = true;
+                    }
+                }
+                if(arr[k] == 'G') {
+                    cnt_G++;
+                    end = k;
+                }
+                if(arr[k] == 'H') {
+                    cnt_H++;
+                    end = k;
+                }
+            }
+            if(cnt_G == 0 || cnt_H == 0 || cnt_G == cnt_H) {
+                if(max_dist < end-first) max_dist = end-first;
+            }
+        }
+    }
+    cout << max_dist;
+    return 0;
+}
