8bd8e76d2794734c37074f7f22b4996b92e617ef
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-06-10 15:34:51 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240610/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 240610/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -5,14 +5,14 @@ int main() {
     int n, k;
     cin >> n >> k;
     if(k > 50) k = 50;
-    int basket[100] = {0,};
+    int basket[110] = {0,};
     while(n--) {
         int cnt, idx;
         cin >> cnt >> idx;
         basket[idx] += cnt;
     }
     int max_candy = 0;
-    for(int i=k; i<100-k; i++) {
+    for(int i=k; i<=100-k; i++) {
         int candy = 0;
         for (int p=i-k; p<=i+k; p++) {
             candy += basket[p];
