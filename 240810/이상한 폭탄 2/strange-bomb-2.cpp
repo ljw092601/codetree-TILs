4267aef85ad7a915f77f1fb181bc817eb0ba4326
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-08-10 18:05:11 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> boom_vec(N, 0);
+    for (int i=0; i<N; i++) {
+        cin >> boom_vec[i];
+    }
+    int max_num_boom = 0;
+    for (int i=0; i<N; i++) {
+        int num_boom = 0;
+        for(int p=i-K; p<=i+K; p++) {
+            if (0 <= p && p < 6 && p != i) {
+                if(boom_vec[p] == boom_vec[i]) num_boom = boom_vec[i];
+            }
+        }
+        if (max_num_boom < num_boom) max_num_boom = num_boom;
+    }
+
+    cout << max_num_boom;
+    return 0;
+}
