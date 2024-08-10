c562e14a10ce23bb59f15a4111638c63745d9709
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-08-10 14:58:49 +0900

    Title: 상해버린 치즈, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/상해버린 치즈/rotten-cheese.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int people_num, cheese_num, eat_num, ill_num;
+    cin >> people_num >> cheese_num >> eat_num >> ill_num;
+    vector<int> people(eat_num, 0);
+    vector<int> cheess(eat_num, 0);
+    vector<int> eat_time(eat_num, 0);
+    for (int i=0; i<eat_num; i++) {
+        cin >> people[i] >> cheess[i] >> eat_time[i];
+    }
+    vector<int> ill_people(ill_num, 0);
+    vector<int> ill_time(ill_num, 0);
+    for (int i=0; i<ill_num; i++) {
+        cin >> ill_people[i] >> ill_time[i];
+    }
+
+    int max_med = 0;
+    for(int i=1; i<=cheese_num; i++) {
+        int how_many_eat = 0;
+        for(int p=0; p<ill_num; p++) {
+            for (int k=0; k<eat_num; k++) {
+                if(cheess[k] == i && people[k] == ill_people[p] && eat_time[k] < ill_time[p]) how_many_eat++;
+            }
+        }
+        if (how_many_eat == ill_num) {
+            int need_med = 0;
+            for (int k=0; k<eat_num; k++) {
+                if(cheess[k] == i) need_med++;
+            }
+            if(max_med < need_med) max_med = need_med;
+        }
+    }
+
+    cout << max_med;
+
+    return 0;
+}
