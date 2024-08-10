5bd9a72fdf8ae2cd218b5431500fc697d468f7f7
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-08-10 15:39:34 +0900

    Title: 개발자의 순위, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/개발자의 순위/developer's-rank.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int K, N;
+    cin >> K >> N;
+    vector<vector<int>> match;
+    for(int i=0; i<K; i++) {
+        vector<int> grade(N, 0);
+        for(int p=0; p<N; p++) {
+            cin >> grade[p];
+        }
+        match.emplace_back(grade);
+    }
+
+    int pair = 0;
+    for(int i=0; i<N; i++) {
+        for(int p=0; p<N; p++) {
+            if (i != p) {
+                bool satisfy = true;
+                for (auto g : match) {
+                    for(auto s : g) {
+                        if(s == i) break;
+                        if(s == p) {
+                            satisfy = false;
+                            break;
+                        }
+                    }
+                    if (!satisfy) break;
+                }
+                if (satisfy) pair++;
+            }
+        }
+    }
+    cout << pair;
+    return 0;
+}
