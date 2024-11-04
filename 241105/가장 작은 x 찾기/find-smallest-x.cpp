8c667e23b4048408e16185b3a833bba9dc58a3e4
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-11-05 00:23:29 +0900

    Title: 가장 작은 x 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241105/가장 작은 x 찾기/find-smallest-x.cpp
+++ 241105/가장 작은 x 찾기/find-smallest-x.cpp
@@ -10,7 +10,7 @@ int main() {
     for(int i=0; i<n; i++) {
         cin >> min_vec[i] >> max_vec[i];
     }
-    for (int i=1; i<=10; i++) {
+    for (int i=min_vec[0]/2; i<=max_vec[0]/2; i++) {
         int predict = i;
         bool fail = false;
         for (int p=0; p<n; p++) {
