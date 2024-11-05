bd8fbf9f9414d8c5d6ef3770aea55f5076313888
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-11-05 17:47:34 +0900

    Title: 훌륭한 점프, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241105/훌륭한 점프/great-jump.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> vec(n);
+    for (int i=0; i<n; i++) {
+        cin >> vec[i];
+    }
+    auto min_max = minmax_element(vec.begin(), vec.end());
+    int min_vec = *min_max.first;
+    int max_vec = *min_max.second;
+
+    for (int i=min_vec; i<=max_vec; i++) {
+        int jump = 0;
+        bool fail = false;
+        for (int v : vec) {
+            if (v <= i) {
+                if (jump > k) {
+                    fail = true;
+                    break;
+                }
+                jump = 0;
+            }
+            jump++;
+        }
+        if (!fail && jump <= k) {
+            cout << i;
+            return 0;
+        }
+    }
+    return 0;
+}
