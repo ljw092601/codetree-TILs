177fcfb4eb33af10c037986121db8f6c93533541
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-11-10 12:43:11 +0900

    Title: 이상한 폭탄 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241110/이상한 폭탄 3/strange-bomb-3.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> vec(n);
+    for (int i=0; i<n; i++) {
+        cin >> vec[i];
+    }
+    return 0;
+}
