338d907db19ac782d604c9b8e94eab2a0ccdb36a
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-11-15 23:36:33 +0900

    Title: 비둘기와 전기줄, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241115/비둘기와 전기줄/pigeons-and-electric-cords.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Pigeon {
+    int num;
+    int state;
+};
+
+int main() {
+    int n;
+    cin >> n;
+    int over = 0;
+    vector<Pigeon> vec;
+    while(n--) {
+        Pigeon pigeon;
+        cin >> pigeon.num >> pigeon.state;
+        bool in_vec = false;
+        for (Pigeon p : vec) {
+            if (p.num == pigeon.num) {
+                in_vec = true;
+                if (p.state != pigeon.state) over++;
+            }
+        }
+        if (!in_vec) vec.push_back(pigeon);
+    }
+    cout << over;
+    return 0;
+}
