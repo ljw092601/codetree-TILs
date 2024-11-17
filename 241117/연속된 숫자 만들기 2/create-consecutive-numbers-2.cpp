e3ed2408af44dcd040205c3358ba309a57b2dbd0
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-11-17 23:18:34 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241117/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 241117/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main() {
     int a,b,c;
     cin >> a >> b >> c;
-    if (abs(a-b) == 1 && abs(b-c) == 1) cout << 0;
-    else if(abs(a-b) == 2 && abs(b-c) == 2) cout << 1;
+    if (abs(a-b) == 1 && abs(b-c) == 1 ||
+        abs(a-b) == 1 && abs(a-c) == 1 ||
+        abs(c-b) == 1 && abs(a-c) == 1) cout << 0;
+    else if(abs(a-b) == 2 || abs(b-c) == 2 || abs(a-c) == 2) cout << 1;
     else cout << 2;
     return 0;
 }
