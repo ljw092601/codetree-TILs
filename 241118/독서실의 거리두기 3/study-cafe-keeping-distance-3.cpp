d4185c96a8d5826bac5457a99ff7928cf8caff7f
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-11-18 22:43:57 +0900

    Title: 독서실의 거리두기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241118/독서실의 거리두기 3/study-cafe-keeping-distance-3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int max_space(const vector<int>& vec) {
+  int result = 0;
+  int space = 0;
+  for (int n : vec) {
+    if(n) {
+      if (result < space) result = space;
+      space = 0;
+    }
+    else space++;
+  }
+  return result;
+}
+
+int min_space(const vector<int>& vec) {
+  int result = vec.size();
+  int space = vec.size();
+  for (int n : vec) {
+    if(n) {
+      if (result > space) result = space;
+      space = 0;
+    }
+    else space++;
+  }
+  return result;
+}
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+    vector<int> seat;
+    for(char c: s) {
+      seat.emplace_back(c - '0');
+    }
+
+    if (max_space(seat) >= min_space(seat)*2 + 1) cout << min_space(seat)+1;
+    else cout << max_space(seat) / 2 +1;
+
+    return 0;
+}
