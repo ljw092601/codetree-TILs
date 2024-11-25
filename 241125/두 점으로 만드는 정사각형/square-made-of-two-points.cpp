c9e3a99b7b8bae3de6fb969d45e2f7a1650d626c
이정원 <143209841+ljw092601@users.noreply.github.com>
2024-11-25 14:36:38 +0900

    Title: 두 점으로 만드는 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241125/두 점으로 만드는 정사각형/square-made-of-two-points.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> x(4);
+    vector<int> y(4);
+    for(int i=0; i<4; i++) {
+        cin >> x[i] >> y[i];
+    }
+    sort(x.begin(), x.end());
+    sort(y.begin(), y.end());
+    cout << max((x[3] - x[0]), (y[3] - y[0])) * max((x[3] - x[0]), (y[3] - y[0]));
+    return 0;
+}
