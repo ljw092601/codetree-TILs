7fa12a9050f96cd4ea57ccdd982861f18da2df49
이정원 <143209841+ljw092601@users.noreply.github.com>
2025-01-05 16:03:05 +0900

    Title: 힙 정렬 구현, Time: 45ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250105/힙 정렬 구현/implement-heap-sort.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100001];
+
+void swap(int a, int b) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+void heapify(int h) {
+    int max = arr[h];
+    int max_i = h;
+
+    if (max < arr[h*2] && h*2 <= n) {
+        max = arr[h*2];
+        max_i = h*2;
+    }
+
+    if (max < arr[h*2+1] && h*2+1 <= n) {
+        max = arr[h*2+1];
+        max_i = h*2+1;
+    }
+
+    if (max_i != h) {
+        swap(h, max_i);
+        heapify(max_i);
+    }
+}
+
+void heap_sort() {
+    for(int i = n/2; i >= 1; i--) {
+        heapify(i);
+    }
+
+    while(n > 1) {
+        swap(1, n);
+        n--;
+        heapify(1);
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+    int size = n;
+
+    heap_sort();
+
+    for (int i = 1; i <= size; i++) {
+        cout << arr[i] << ' ';
+    }
+
+    return 0;
+}
