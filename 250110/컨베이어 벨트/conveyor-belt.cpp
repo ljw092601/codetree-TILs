31bf16a00573f55de3d97a8d94b51bc1fb08f7fc
이정원 <143209841+ljw092601@users.noreply.github.com>
2025-01-10 02:06:33 +0900

    Title: 컨베이어 벨트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250110/컨베이어 벨트/conveyor-belt.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int n, t;
+int u[200];
+int d[200];
+
+void move() {
+    int u_temp = u[n-1];
+    int d_temp = d[n-1];
+    for(int i=n-1; i>0; i--) {
+        u[i] = u[i-1];
+        d[i] = d[i-1];
+    }
+    u[0] = d_temp;
+    d[0] = u_temp;
+
+} 
+
+int main() {
+    cin >> n >> t;
+
+    for (int i = 0; i < n; i++) cin >> u[i];
+
+    for (int i = 0; i < n; i++) cin >> d[i];
+
+    for(int i=0; i<t%6; i++) move();
+
+    for (int i = 0; i < n; i++) cout << u[i] << ' ';
+    cout << endl;
+    for (int i = 0; i < n; i++) cout << d[i] << ' ';
+
+    return 0;
+}
