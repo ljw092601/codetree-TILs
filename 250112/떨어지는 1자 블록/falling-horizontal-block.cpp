8a3b3f6ae0b9ce20ce3bc53711c69160d8b9219f
이정원 <143209841+ljw092601@users.noreply.github.com>
2025-01-12 22:32:45 +0900

    Title: 떨어지는 1자 블록, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250112/떨어지는 1자 블록/falling-horizontal-block.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, k;
+int grid[100][100];
+
+int land_idx() {
+    for(int i=0; i<n; i++) {
+        for(int p=k-1; p<k-1+m; p++) {
+            if(grid[i][p]) return i-1;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m >> k;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    int land = land_idx();
+
+    for(int p=k-1; p<k-1+m; p++) {
+        grid[land][p] = 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << grid[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
