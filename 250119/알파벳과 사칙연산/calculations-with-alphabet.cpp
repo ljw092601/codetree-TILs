9e03dd7c7c5aeb2c58fa82dd9a13ecf44f8ac0b8
이정원 <143209841+ljw092601@users.noreply.github.com>
2025-01-19 23:49:10 +0900

    Title: 알파벳과 사칙연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250119/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string expression;
+
+int values[6];  // a:0, b:1, ... , f:5
+int max_answer = 0;
+
+int val_to_int(char v) {
+    if(v == 'a') return values[0];
+    else if(v == 'b') return values[1];
+    else if(v == 'c') return values[2];
+    else if(v == 'd') return values[3];
+    else if(v == 'e') return values[4];
+    else if(v == 'f') return values[5];
+}
+
+int cal(int a, char s, int b) {
+    if (s == '+') return a + b;
+    else if (s == '-') return a - b;
+    else if (s == '*') return a * b;
+}
+
+int cal_exp() {
+    int cal_val = val_to_int(expression[0]);
+    for(int i=1; i<expression.size(); i += 2) {
+        cal_val = cal(cal_val, expression[i], val_to_int(expression[i+1]));
+    }
+    return cal_val;
+}
+
+void simulation(int n) {
+    if(n == 6) {
+        max_answer = max(max_answer, cal_exp());
+        return;
+    }
+    for(int i=1; i<=4; i++) {
+        values[n] = i;
+        simulation(n+1);
+    }
+}
+
+int main() {
+    cin >> expression;
+
+    simulation(0);
+
+    cout << max_answer;
+
+    return 0;
+}
