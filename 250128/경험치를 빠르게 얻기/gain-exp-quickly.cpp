b338dc2d4345382da83031436ec46123ffea743a
이정원 <143209841+ljw092601@users.noreply.github.com>
2025-01-28 00:55:56 +0900

    Title: 경험치를 빠르게 얻기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250128/경험치를 빠르게 얻기/gain-exp-quickly.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int e[100], t[100];
+int dp[10001];  // dp[x] = x만큼 시간을 쓸때 얻는 최대 경험치
+int temp[10001];
+
+void copy() {
+    for(int i=0; i<=10001; i++) {
+        dp[i] = temp[i];
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> e[i] >> t[i];
+    }
+
+    for(int i=0; i<n; i++) {
+        dp[t[i]] = e[i];
+        temp[t[i]] = e[i];
+        for(int p=0; p<10001; p++) {
+            if(dp[p]) temp[p + t[i]] = max(temp[p + t[i]], dp[p] + e[i]);
+        }
+        copy();
+    }
+
+
+    for(int p=0; p<10001; p++) {
+        if(dp[p] >= m) {
+            cout << p;
+            return 0;
+        }
+    }
+
+    cout << -1;
+
+    return 0;
+}
