ded4f8a18befa2de7ea9c7e2a39211aa371cd38f
이정원 <143209841+ljw092601@users.noreply.github.com>
2025-01-27 21:04:41 +0900

    Title: 동전 거슬러주기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250127/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int coin[100];
+int dp[30000];
+
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> coin[i];
+    }
+
+    for(int i=1; i<=M; i++) {
+        dp[i] = 200;
+    }
+
+    for(int i=0; i<M; i++) {
+        for(int p=0; p<N; p++) {
+            dp[i+coin[p]] = min(dp[i+coin[p]], dp[i] + 1);
+        }
+    }
+
+    cout << dp[M];
+
+    return 0;
+}
