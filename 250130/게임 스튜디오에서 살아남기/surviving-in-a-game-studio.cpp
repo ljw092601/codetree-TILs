c2b3c65c08177fa169e5053701992ca520bcac07
이정원 <143209841+ljw092601@users.noreply.github.com>
2025-01-30 20:14:17 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250130/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.cpp
+++ 250130/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+const int MOD = 1e9 + 7;
 int n;
 int dp[1005][3][3];
 // dp[x][y][z] x번째까지 수열에서 연속된 B 개수 y개, 전체 T개수 z개인 수열의 경우의 수
@@ -16,20 +17,20 @@ int main() {
         for(int p=0; p<3; p++) {
             for(int k=0; k<3; k++) {
                 if(p == 2 && k == 2) {
-                    dp[i+1][0][k] += dp[i][p][k];
+                    dp[i+1][0][k] = (dp[i+1][0][k] + dp[i][p][k]) % MOD;
                 }
                 else if(p == 2) {
-                    dp[i+1][0][k] += dp[i][p][k];
-                    dp[i+1][0][k+1] += dp[i][p][k];
+                    dp[i+1][0][k] = (dp[i+1][0][k] + dp[i][p][k]) % MOD;
+                    dp[i+1][0][k+1] = (dp[i+1][0][k+1] + dp[i][p][k]) % MOD;
                 }
                 else if(k == 2) {
-                    dp[i+1][0][k] += dp[i][p][k];
-                    dp[i+1][p+1][k] += dp[i][p][k];
+                    dp[i+1][0][k] = (dp[i+1][0][k] + dp[i][p][k]) % MOD;
+                    dp[i+1][p+1][k] = (dp[i+1][p+1][k] + dp[i][p][k]) % MOD;
                 }
                 else {
-                    dp[i+1][p+1][k] += dp[i][p][k];
-                    dp[i+1][0][k+1] += dp[i][p][k];
-                    dp[i+1][0][k] += dp[i][p][k];
+                    dp[i+1][p+1][k] = (dp[i+1][p+1][k] + dp[i][p][k]) % MOD;
+                    dp[i+1][0][k+1] = (dp[i+1][0][k+1] + dp[i][p][k]) % MOD;
+                    dp[i+1][0][k] = (dp[i+1][0][k] + dp[i][p][k]) % MOD;
                 }
             }
         }
@@ -38,7 +39,7 @@ int main() {
     int sum = 0;
     for(int i=0; i<3; i++) {
         for(int p=0; p<3; p++) {
-            sum += dp[n][i][p];
+            sum = (sum + dp[n][i][p]) % MOD;
         }
     }
     cout << sum;
