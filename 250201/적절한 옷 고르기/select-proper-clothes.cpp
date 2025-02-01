560be1c2c21a9e000414ab0bd128e71429d4b689
이정원 <143209841+ljw092601@users.noreply.github.com>
2025-02-01 16:53:20 +0900

    Title: 적절한 옷 고르기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250201/적절한 옷 고르기/select-proper-clothes.cpp
+++ 250201/적절한 옷 고르기/select-proper-clothes.cpp
@@ -7,6 +7,15 @@ int s[200], e[200], v[200];
 int dp[200][200];
 // dp[x][y] x일에 마지막으로 입은 옷이 y일때, 얻는 최고 점수
 
+void print() {
+    for(int i=1; i<=M; i++) {
+        for(int p=0; p<N; p++) {
+            cout << dp[i][p] << ' ';
+        }
+        cout << endl;
+    }
+}
+
 bool can_wear(int day, int cloth) {
     return s[cloth] <= day+1 && day+1 <= e[cloth];
 }
@@ -40,9 +49,10 @@ int main() {
     
     int max_dp = 0;
     for(int i=0; i<N; i++) {
-        max_dp = max(max_dp, dp[M][i]);
+        max_dp = max(max_dp, dp[M-1][i]);
     }
     cout << max_dp;
+    //print();
 
     return 0;
 }
