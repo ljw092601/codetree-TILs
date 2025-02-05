d7799df2e043166ac90c87dad850e15d4d62b538
이정원 <143209841+ljw092601@users.noreply.github.com>
2025-02-05 19:45:11 +0900

    Title: 수정 수집하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250205/수정 수집하기/collect-crystals.cpp
+++ 250205/수정 수집하기/collect-crystals.cpp
@@ -1,42 +1,63 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
 int n, k;
 string str;
-int dp[1005][2][25];
+int dp[1001][2][21];
 // dp[x][y][z] x번째 수정이 떨어지고 현재 y위치에 있고 z번 움직였을때 얻을 수 있는 최대 수정
+
+void init() {
+    for(int i=0; i<1001; i++) {
+        for(int p=0; p<2; p++) {
+            for(int k=0; k<21; k++) {
+                dp[i][p][k] = INT_MIN;
+            }
+        }
+    }
+}
+
+void print(int i) {
+    for(int p=0; p<2; p++) {
+        for(int k=0; k<12; k++) {
+            cout << (dp[i][p][k] < 0 ? -1 : dp[i][p][k]) << ' ';
+        }
+        cout << endl;
+    }
+}
 
 int main() {
     cin >> n >> k;
     cin >> str;
+    init();
 
     if(str[0] == 'L') {
-        dp[0][0][0] = 2;
+        dp[0][0][0] = 1;
     }
     else {
-        dp[0][0][0] = 1;
-        dp[0][1][1] = 2;
+        dp[0][0][0] = 0;
+        dp[0][1][1] = 1;
     }
     
 
     for(int i=1; i<n; i++) {
         for(int p=0; p<=k; p++) {
             if(str[i] == 'L') {
-                if(dp[i-1][0][p]) {
+                if(0 <= dp[i-1][0][p]) {
                     dp[i][0][p] = max(dp[i][0][p], dp[i-1][0][p] + 1);
                 }
-                else if(dp[i-1][1][p] && p < k) {
-                    dp[i][0][p+1] = max(dp[i][0][p+1], dp[i-1][1][p] + 1);
+                else if(0 <= dp[i-1][1][p]) {
+                    if(p<k) dp[i][0][p+1] = max(dp[i][0][p+1], dp[i-1][1][p] + 1);
                     dp[i][1][p] = max(dp[i][1][p], dp[i-1][1][p]);
                 }
             }
             else {
-                if(dp[i-1][1][p]) {
+                if(0 <= dp[i-1][1][p]) {
                     dp[i][1][p] = max(dp[i][1][p], dp[i-1][1][p] + 1);
                 }
-                else if(dp[i-1][0][p] && p < k) {
-                    dp[i][1][p+1] = max(dp[i][1][p+1], dp[i-1][0][p] + 1);
+                else if(0 <= dp[i-1][0][p]) {
+                    if(p<k) dp[i][1][p+1] = max(dp[i][1][p+1], dp[i-1][0][p] + 1);
                     dp[i][0][p] = max(dp[i][0][p], dp[i-1][0][p]);
                 }
             }
@@ -48,7 +69,12 @@ int main() {
         max_dp = max(max_dp, dp[n-1][0][i]);
         max_dp = max(max_dp, dp[n-1][1][i]);
     }
-    cout << max_dp - 1;
+    // print(0);
+    // print(1);
+    // print(2);
+    // print(3);
+    // print(4);
+    cout << max_dp;
 
     return 0;
 }
