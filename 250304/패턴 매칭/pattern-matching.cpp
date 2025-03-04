f31e7aea6e16d36a858051e86820adebd6fb905b
이정원 <143209841+ljw092601@users.noreply.github.com>
2025-03-04 18:03:03 +0900

    Title: 패턴 매칭, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250304/패턴 매칭/pattern-matching.cpp
+++ 250304/패턴 매칭/pattern-matching.cpp
@@ -5,6 +5,10 @@ using namespace std;
 string s;
 string p;
 
+bool same(int si, int pi) {
+    return s[si] == p[pi] || p[pi] == '.';
+}
+
 int main() {
     cin >> s;
     cin >> p;
@@ -14,20 +18,26 @@ int main() {
 
     int si = 0;
     int pi = 0;
-    while(si < a && pi < b) {
-        if(p[pi] == '*') {
-            pi++;
-        }
-        else if(s[si] == p[pi] || p[pi] == '.') {
-            si++;
-            pi++;
+    while(si < a || pi < b) {
+        if(p[pi + 1] == '*') {
+            while(same(si, pi) && si < a) {
+                si++;
+            }
+            pi += 2;
         }
         else {
-            si++;
+            if(same(si, pi)) {
+                si++;
+                pi++;
+            }
+            else {
+                cout << "false";
+                return 0;
+            }
         }
     }
 
-    cout << (pi == b ? "true" : "false");
+    cout << "true";
 
     return 0;
 }
